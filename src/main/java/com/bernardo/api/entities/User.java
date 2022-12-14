fd4e1194a88af369b58ef62b6cbe4d9935b587c4
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-12-14 11:17:24 -0300

    feat: add Order entity

--- src/main/java/com/bernardo/api/entities/User.java
+++ src/main/java/com/bernardo/api/entities/User.java
@@ -1,11 +1,10 @@
 package com.bernardo.api.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -18,6 +17,9 @@ public class User {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -69,6 +71,10 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
