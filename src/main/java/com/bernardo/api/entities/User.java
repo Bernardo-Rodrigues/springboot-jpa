ff7322916ddc29fceb9dc01cd28a097633986c03
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-12-14 08:40:20 -0300

    feat: add H2 database and JPA dependency

--- src/main/java/com/bernardo/api/entities/User.java
+++ src/main/java/com/bernardo/api/entities/User.java
@@ -1,9 +1,18 @@
 package com.bernardo.api.entities;
 
 import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
