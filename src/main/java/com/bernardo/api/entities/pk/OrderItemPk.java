668d2ba285d897db9d564705777087f05fade708
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-12-20 13:57:54 -0300

    feat: add OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/bernardo/api/entities/pk/OrderItemPk.java
@@ -0,0 +1,48 @@
+package com.bernardo.api.entities.pk;
+
+import com.bernardo.api.entities.Order;
+import com.bernardo.api.entities.Product;
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPk {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPk that = (OrderItemPk) o;
+        return order.equals(that.order) && product.equals(that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
