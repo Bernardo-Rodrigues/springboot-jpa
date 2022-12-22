ccfdfb5a54ebc6e1d9860258f872c2e196ae60fd
Bernardo Rodrigues <bernardo.rodrigues@capco.com>
2022-12-22 10:37:01 -0300

    feat: add resource not found exception handling

---
+++ src/main/java/com/bernardo/api/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bernardo.api.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
--- src/main/java/com/bernardo/api/services/UserService.java
+++ src/main/java/com/bernardo/api/services/UserService.java
@@ -1,6 +1,7 @@
 package com.bernardo.api.services;
 
 import com.bernardo.api.entities.User;
+import com.bernardo.api.exceptions.ResourceNotFoundException;
 import com.bernardo.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,7 @@ public class UserService {
     }
 
     public User findById(Integer id){
-        return userRepository.findById(id).get();
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
